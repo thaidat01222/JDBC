8437a1bc992394fa43d7a44960e39145b8a49f7d
thaidat01222 <thaidat01222@gmail.com>
2020-04-06 01:20:12 +0700

    Bổ sung class student

---
+++ src/Student.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Student {
+    private int ID ;
+    private String Name;
+    private float Math;
+    private float Phys;
+    private float Chem;
+    private float Aver;
+    public Student(int ID, String Name, float Math, float Phys, float Chem, float Aver){
+        this.ID = ID;
+        this.Name = Name;
+        this.Math = Math;
+        this.Phys = Phys;
+        this.Chem = Chem;
+        this.Aver = Aver;
+    }
+    public void setID(int id) {
+        this.ID = ID;
+    }
+    public int getID(){
+        return  ID;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public String getName(){
+        return  Name;
+    }
+    public void setMath(float Math) {
+        this.Math = Math;
+    }
+    public float getMath(){
+        return  Math;
+    }
+
+    public void setPhys(float Phys) {
+        this.Phys = Phys;
+    }
+    public float getPhys(){
+        return  Phys;
+    }
+    public void setChem(float Chem) {
+        this.Chem = Chem;
+    }
+    public float getChem(){
+        return  Chem;
+    }
+    public void setAver(float Aver) {
+        this.Aver = Aver;
+    }
+    public float getAver(){
+        return  Aver;
+    }
+
+}
