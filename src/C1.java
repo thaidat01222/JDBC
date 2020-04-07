62f959187b22d97d5fa4a7f17e8ed580137c5d21
thaidat01222 <thaidat01222@gmail.com>
2020-04-07 22:49:14 +0700

    Sửa hàm update, hàm add, hàm delete

--- src/C1.java
+++ src/C1.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class C1 {
     public Connection getConnection() throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentmanagement?serverTimezone=UTC", "root", "root");
         return conn;
     }
@@ -16,7 +16,7 @@ public class C1 {
         return rs;
     }
 
-    public Student getStudentByID(int id) throws ClassNotFoundException, SQLException {
+    private Student getStudentByID(int id) throws ClassNotFoundException, SQLException {
         Connection conn = getConnection();
         PreparedStatement ps = conn.prepareStatement("SELECT  *from student where id = ?");
         ps.setFloat(1, id);
@@ -40,34 +40,40 @@ public class C1 {
         return s;
     }
 
-    /*private void updateStudentByID(int id) throws ClassNotFoundException, SQLException {
-            Connection conn = getConnection();
-            PreparedStatement ps = conn.prepareStatement("UPDATE student SET ? = ? where id = ?");
-            ps.setInt(1,);
-            ps.setInt(2,);
-            ps.setInt(3,id);
-            ResultSet rs =getResultSet(ps);
+    private void updateStudentByID(int id, String x , String y) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("UPDATE student SET ? = ? where id = ?");
+        ps.setString(1,x);
+        ps.setString(2,y);
+        ps.setInt(3,id);
+        ResultSet rs = getResultSet(ps);
+        mapResultSetToStudent(rs);
     }
 
-    private void addStudent()throws ClassNotFoundException, SQLException {
+    private void addStudent(int x, String y, float z, float t, float q)throws ClassNotFoundException, SQLException {
 
         Connection conn = getConnection();
-
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO student(ID, Name , Math, Phys, Chem ,Aver) VALUES (?,?,?,?,?,?)");
-        ps.setInt(1, ID);
-        ps.setString(2, Name);
-        ps.setFloat(3, Math);
-        ps.setFloat(4, Phys);
-        ps.setFloat(5, Chem);
-        ps.setFloat(6, (Math + Phys + Chem)/3);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO student VALUES (?,?,?,?,?,?)");
+        ps.setInt(1,x);
+        ps.setString(2,y);
+        ps.setFloat(3,z);
+        ps.setFloat(4,t);
+        ps.setFloat(5,q);
+        ps.setFloat(6,(z+t+q)/3);
+        ps.executeUpdate();
         ResultSet rs = getResultSet(ps);
+        //mapResultSetToStudent(rs);
     }
     private void deleteStudentById(int id) throws ClassNotFoundException, SQLException {
-
-    }*/
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("Delete table student where id = ?");
+        ps.setInt(1,id);
+        ps.executeUpdate();
+        
+    }
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         C1 c = new C1();
-        Student c1 = c.getStudentByID(1);
-        c1.Display();
+        //Student c1 = c.getStudentByID(1);
+        //c1.Display();
     }
 }
